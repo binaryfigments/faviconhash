9c44387b8c6e804fa8a5ef66daad24fa3970f357
Sebastian Broekhoven <s.broekhoven@gmail.com>
2021-03-02 22:22:34 +0100

    Example URL changed

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # faviconhash
 Get the favicon hash for fun and profit.
 
-Start for now
+Installation:
+
+```shell
+GO111MODULE=on go get -v github.com/binaryfigments/faviconhash/cmd/faviconhash
+```
+
+Help:
+
+```shell
+$ faviconhash -h
+Usage of faviconhash:
+  -insecure
+    	Allow insecure HTTPS requests (default true)
+  -uri string
+    	Set Request URI (default "https://example.nl/favicon.ico")
+  -useragent string
+    	Set User-Agent (default "Mozilla/5.0 (compatible; FaviconHash/0.1; +https://github.com/binaryfigments/faviconhash)")
+```
--- cmd/faviconhash/faviconhash.go
+++ cmd/faviconhash/faviconhash.go
@@ -10,7 +10,7 @@ import (
 func parseOptions() *faviconhash.Options {
 	options := &faviconhash.Options{}
 
-	flag.StringVar(&options.RequestURI, "uri", "https://hivemanager.databyte.nl/hm/images/favicon.ico", "Set Request URI")
+	flag.StringVar(&options.RequestURI, "uri", "https://example.nl/favicon.ico", "Set Request URI")
 	flag.StringVar(&options.UserAgent, "useragent", "Mozilla/5.0 (compatible; FaviconHash/0.1; +https://github.com/binaryfigments/faviconhash)", "Set User-Agent")
 	flag.BoolVar(&options.Insecure, "insecure", true, "Allow insecure HTTPS requests")
 	flag.Parse()
